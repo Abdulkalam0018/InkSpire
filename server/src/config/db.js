44beff4d5609ae3c34ee4a50b613b017dac4a1da
Manjeet <yadavmanjeet629@gmail.com>
2026-04-02 16:22:12 +0530

    delete node modules and add gitignore

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-  const uri = process.env.MONGODB_URI;
-  if (!uri) {
-    throw new Error("MONGODB_URI is missing");
+  try {
+    const uri = process.env.MONGODB_URI;
+    const dbname = process.env.DB_NAME;
+    if (!uri) {
+      throw new Error("MONGODB_URI is missing");
+    }
+    await mongoose.connect(`${uri}/${dbname}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
   }
-  await mongoose.connect(uri);
   console.log("MongoDB connected");
 }
