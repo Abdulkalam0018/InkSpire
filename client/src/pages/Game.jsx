73824f31be9bfa87d7a5a032db4599113415b101
Abdul kalam <166041376+Abdulkalam0018@users.noreply.github.com>
2026-04-13 10:17:05 +0000

    feat: update game layout styles for improved responsiveness and structure

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -1,31 +1,41 @@
 import { GameProvider } from "../context/GameContext.jsx";
+import { useGame } from "../context/GameContext.jsx";
 import GameTopBar from "../components/game/GameTopBar.jsx";
 import GameWordCard from "../components/game/GameWordCard.jsx";
 import GameGuessCard from "../components/game/GameGuessCard.jsx";
 import GameCanvasCard from "../components/game/GameCanvasCard.jsx";
 import GameScoreboardCard from "../components/game/GameScoreboardCard.jsx";
 
+function GameLayoutContent() {
+  const { gameState } = useGame();
+  const shouldShowWordCard = gameState?.status === "presenter-choosing";
+
+  return (
+    <div className="game-page">
+      <GameTopBar />
+
+      <div className="game-layout">
+        <aside className="game-left">
+          <GameScoreboardCard />
+        </aside>
+
+        <section className="game-center">
+          {shouldShowWordCard ? <GameWordCard /> : null}
+          <GameCanvasCard />
+        </section>
+
+        <aside className="game-right">
+          <GameGuessCard />
+        </aside>
+      </div>
+    </div>
+  );
+}
+
 export default function Game() {
   return (
     <GameProvider>
-      <div className="game-page">
-        <GameTopBar />
-
-        <div className="game-layout">
-          <aside className="game-left">
-            <GameScoreboardCard />
-          </aside>
-
-          <section className="game-center">
-            <GameWordCard />
-            <GameCanvasCard />
-          </section>
-
-          <aside className="game-right">
-            <GameGuessCard />
-          </aside>
-        </div>
-      </div>
+      <GameLayoutContent />
     </GameProvider>
   );
 }
